2c824aaddd1fddd18a1fc50965cf449b08ca58bd
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-10 11:51:02 -0400

    Completed: Register and Login routes, ANSWERS.md
    See comments on code for explainers
    Still need to find way to share the jwtKey from /_secrets/keys.js
    Moving on to UI for login and register

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,6 +2,18 @@
 
 1.  Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
 
+`middleware`, as it relates to the authentication we've been doing, is code written to check for sessions that then gets placed in front of endpoints we want to restrict
+
+`sessions` allow a server to store information about a client - they provide a way to persist data across requests
+
+`bcrypt`  - see below
+
+`jwt` defines a compact and self-contained way for securely transmitting information between parties as a JSON object
+
 2.  What does bcrypt do in order to prevent attacks?
 
+`bcrypt` salts and hashes passswords to prevent rainbow table attacks
+
 3.  What are the three parts of the JSON Web Token?
+
+`payload`, `secret`, `options`
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const jwt = require('jsonwebtoken');
+const uuidv4 = require('uuid/v4');
 
 const { authenticate } = require('./middlewares');
 
@@ -6,15 +10,73 @@ module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/api/users', getUsers);
 };
+
+const secret = 'I can keep a screet'; // global so it's accessible anywhere
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h',
+    jwtid: uuid() // random jwtid
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+	const hash = bcrypt.hashSync(user.password, 10); // Auto-gen a salt and hash
+	user.password = hash; // store hash in password DB
+
+    db('users') // go into users
+        .insert(user) // insert the new user ( with hashed pw)
+        .then(ids => {
+            db('users')
+                .where({ id: ids[0] }) // find the appropriate user
+                .first() // the first one
+                .then(user => {
+                const token = generateToken(user); // generate the token
+                    res.status(201).json(token); // return new token
+                });
+        })
+        .catch(err => {
+            res.status(500).json(err); // throw err if it fails
+        });
 }
 
 function login(req, res) {
   // implement user login
+  const credentials = req.body;
+
+    db('users') // go into users
+        .where({username: credentials.username}) // find the user by username
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(credentials.password, user.password)) { // compare the found user's pw to the pw in the db
+                const token = generateToken(user); // generate the token
+                res.send(token) // return the token
+            } else {
+                res.status(401).json({ error: 'You shall not pass'}) // throw this error if the pw doesn't match
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 }
+
+function getUsers(req, res) {
+  db('users')
+      .then(users => {
+          res.status(200).json(users);
+      })
+      .catch(err => res.status(500).json(err));
+};
 
 function getJokes(req, res) {
   axios
